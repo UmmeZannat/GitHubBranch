ffb2997032586f7df688263c9f6ca2dd3421ae25
UmmeZannat <76455582+UmmeZannat@users.noreply.github.com>
2020-12-24 12:30:18 -0500

    Eclipse changes

--- GitHubBranch/src/main/java/com/pagesbranch/LoginPageBranch.java
+++ GitHubBranch/src/main/java/com/pagesbranch/LoginPageBranch.java
@@ -3,6 +3,7 @@ package com.pagesbranch;
 public class LoginPageBranch {
 	public void login() {
 		System.out.println("This is login method");
+		System.out.println("This is login method 2");
 	}
 
 }
